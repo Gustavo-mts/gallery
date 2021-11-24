383d4a25751827b9e31f080f4330deba7422098d
Gustavo Martins <gm357191@gmail.com>
2021-11-24 18:41:21 -0300

    footer

--- src/Components/partials/Footer/index.tsx
+++ src/Components/partials/Footer/index.tsx
@@ -4,7 +4,16 @@ import './styled.scss';
 function Footer() {
     return (
         <>
-            <h2 className="footer">Footer</h2>
+            <div className="footer">
+                <div className="footer--copy">
+                    <div className="footer--copy-photos">
+                        <p>Direito das fotos reservados para <a href="https://www.pexels.com/pt-br/" target="_blank">Pexels©</a></p>
+                    </div>
+                    <div className="footer--copy-template">
+                        <p>Direito do template reservado para <a href="https://github.com/Gustavo-mts" target="_blank">Gustavo Martins©</a> </p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
