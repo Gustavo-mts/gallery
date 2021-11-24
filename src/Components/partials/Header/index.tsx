8a11d2077e63caddea84493bd244852a13e65241
Gustavo Martins <gm357191@gmail.com>
2021-11-24 10:42:14 -0300

    estilizando o header

--- src/Components/partials/Header/index.tsx
+++ src/Components/partials/Header/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './styled.scss';
 
+
 function Header() {
     return (
         <>
-            <h2 className="header">Header</h2>
+            <h2 className="header">
+                <div className="header--area">
+                    <div className="header--title">
+                        <h1>My Album</h1>
+                    </div>
+                    <div className="header--search">
+                        <input
+                            type="text"
+                            placeholder="O quê você procura?"
+                        />
+                    </div>
+                </div>
+            </h2>
         </>
     );
 }
