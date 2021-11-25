63e50e6cba3412c5b09daab969a1726a498ca2f9
Gustavo Martins <gm357191@gmail.com>
2021-11-25 11:22:35 -0300

    album - requisiçao

--- src/Components/Services/Api.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Api() {
-    return (
-        <>...</>
-    );
-}
-
-export default Api;
--- src/Components/partials/Album/index.tsx
+++ src/Components/partials/Album/index.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-function Album() {
-    return (
-        <>
-            <h2>Album</h2>
-        </>
-    );
+
+export default function App() {
+  const [user, setUser] = useState();
+
+  useEffect(()=>{
+    function getPhotos (images:any) {
+      images.map ((image:any) => { 
+      console.log (image) 
+      }) 
+    }
+     fetch("https://api.pexels.com/v1/search?query=people",{
+       headers: {
+        Authorization: '563492ad6f91700001000001623b9853bf6e460eb6ef3ad569eb07e8'
+       }
+     })
+        .then(resp => {
+          return resp.json()
+        })
+        .then(data => {
+          getPhotos(data.photos);
+        })
+  }, [])
+
+  return (
+    <>
+      ...
+    </>
+  );
 }
-
-export default Album;
