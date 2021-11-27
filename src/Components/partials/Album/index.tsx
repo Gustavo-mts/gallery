c283545674f69e0691a7974652e22541120d2409
Gustavo Martins <gm357191@gmail.com>
2021-11-26 23:58:42 -0300

    photos

--- src/Components/partials/Album/index.tsx
+++ src/Components/partials/Album/index.tsx
@@ -1,31 +1,42 @@
 import React, { useEffect, useState } from "react";
+import './styled.scss';
 
-
-export default function App() {
-  const [user, setUser] = useState();
+function App() {
+  const photo:any = [];
+  const [images, setImages] = useState([]);
+  const [imagens, setImagens] = useState([])
 
   useEffect(()=>{
     function getPhotos (images:any) {
       images.map ((image:any) => { 
-      console.log (image) 
+      photo.push(image.src.original)
       }) 
+      setImagens(photo)
     }
      fetch("https://api.pexels.com/v1/search?query=people",{
        headers: {
         Authorization: '563492ad6f91700001000001623b9853bf6e460eb6ef3ad569eb07e8'
        }
      })
-        .then(resp => {
-          return resp.json()
-        })
-        .then(data => {
-          getPhotos(data.photos);
-        })
+     .then(resp => {
+        return resp.json()
+     })
+     .then(data => {
+      getPhotos(data.photos);
+     })
   }, [])
 
   return (
     <>
-      ...
+      <div className="Album">
+        {photo && 
+          imagens.map((i,k)=>
+            <img src={i} />
+          )
+        }
+      </div>     
     </>
   );
 }
+
+export default App;
